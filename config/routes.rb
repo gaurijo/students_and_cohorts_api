8e843921bb84fcfacecbd866b0f0afdaddc103b8
Gauri Joshi <gauri.joshi@planetargon.com>
2023-04-27 16:10:15 -0700

    Fixes correct api endpoint in routes and in test file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      get '/cohorts/:id/students', to: 'students#index'
+      get '/students', to: 'students#index'
     end
   end
 end
