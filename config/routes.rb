23882d5a7d18bf437dc1cd793d38b1c4a49a7d48
Gauri Joshi <gauri.joshi@planetargon.com>
2023-05-01 12:37:22 -0700

    Update get request - Cohorts Controller to show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get '/students', to: 'students#index'
+      get '/cohorts/:id', to: 'cohorts#show'
     end
   end
 end
