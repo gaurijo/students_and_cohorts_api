35985b90a3c2d31a78f38cd9594dec7447a6f48d
Gauri Joshi <gauri.joshi@planetargon.com>
2023-05-01 12:36:36 -0700

    Test: Find cohort by id

---
+++ spec/requests/api/v1/cohorts_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Cohorts API' do 
+
+  it 'has a list of cohorts based on id' do
+    cohort_2107_be = Cohort.create(name: "2107 BE", start_date: Date.parse('01-07-2021'), graduation_date: Date.parse('01-02-2022') )
+    cohort_2103_fe = Cohort.create(name: "2103 FE", start_date: Date.parse('01-03-2021'), graduation_date: Date.parse('01-10-2021') )
+    cohort_2105_be = Cohort.create(name: "2105 BE", start_date: Date.parse('01-05-2021'), graduation_date: Date.parse('01-12-2021') )
+    cohort_2009_fe = Cohort.create(name: "2009 FE", start_date: Date.parse('01-09-2020'), graduation_date: Date.parse('01-04-2021') )
+
+    get "/api/v1/cohorts/#{cohort_2107_be.id}"
+
+    expect(response).to be_successful
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    # require 'pry'; binding.pry 
+  end
+end
